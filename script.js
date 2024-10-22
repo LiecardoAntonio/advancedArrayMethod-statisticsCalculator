6bcbe1cdae81f984606f424f581f313964e35285
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-22 21:37:50 +0700

    progress3

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const calculate = () => {
   const value = document.querySelector('#numbers').value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 };
 
 // before using implicit return
@@ -31,6 +33,30 @@ function getMedian(array) {
   return sorted[Math.floor(sorted.length/2)];
 }
 
+
+const getMode = (array) => {
+  const counts = {};
+  // array.forEach(el => {
+  //   if(counts[el]) {
+  //     counts[el]++;
+  //   } else {
+  //     counts[el] = 1;
+  //   }
+  // })
+  array.forEach(el => counts[el] = (counts[el] || 0) + 1);
+
+  if(new Set(Object.values(counts)).size===1){
+    return null;
+  } //In the condition, create a Set with new Set() and pass it the Object.values() of your counts object. If the size property of this Set is equal to 1, that tells you every value appears the same number of times. In this case, return null from your function.
+
+  console.log(Object.keys(counts));
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+
+  const mode = Object.keys(counts).filter((el) => counts[el]===counts[highest]);
+
+  return mode.join(', '); //mode is an array, so return it as a string with the .join() method. Separate the elements with a comma followed by a space.
+}
+
 //testing arr method
 const testArr = [1,2,3,4,5];
 console.log(testArr.map(el => el+=1));
@@ -48,3 +74,15 @@ const secondMiddleNumber = testArr2[(testArr2.length/2)-1];
 
 const evenListMedian = getMean([firstMiddleNumber, secondMiddleNumber]);
 console.log(evenListMedian);
+
+//testing Set
+const set1 = new Set([1, 2, 2, 3, 4, 5]);
+console.log(set1)
+
+const a = new Set([1, 2, 3]);
+const b = new Map([
+  [1, "one"],
+  [2, "two"],
+  [4, "four"],
+]);
+console.log(a.union(b));
